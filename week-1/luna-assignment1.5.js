c7556d7cdff95e5197cbc01a470ee05f062f9b74
Adam Luna <adluna@my365.bellevue.edu>
2020-10-25 21:15:19 -0500

    add code for 1.5 assignment

--- week-1/luna-assignment1.5.js
+++ week-1/luna-assignment1.5.js
@@ -7,3 +7,41 @@
 ; Description: Types, Values and Variables JavaScript assignment for WEB231.
 ; ======================================== 
 */
+// First employee
+var firstName = 'Darth';
+var lastName = 'Vader';
+var address = '123 Deathstar Blvd.';
+var payRate = 100.55;
+var hireDate = new Date(2001, 4, 15);
+
+console.log('First Name: ' + firstName);
+console.log('Last Name: ' + lastName);
+console.log('Address: ' + address);
+console.log('Pay Rate: ' + payRate.toFixed(1));
+console.log('Hire Date: ' + hireDate.toLocaleDateString());
+
+// Second employee
+var firstName = 'Luke';
+var lastName = 'Skywalker';
+var address = '123 Force Ave.';
+var payRate = 55.75;
+var hireDate = new Date(1998, 3, 12);
+
+console.log('First Name: ' + firstName);
+console.log('Last Name: ' + lastName);
+console.log('Address: ' + address);
+console.log('Pay Rate: ' + payRate.toFixed(1));
+console.log('Hire Date: ' + hireDate.toLocaleDateString());
+
+// Third employee
+var firstName = 'Rey';
+var lastName = 'Skywalker';
+var address = '123 Lastjedi Trl.';
+var payRate = 25.65;
+var hireDate = new Date(2019, 7, 3);
+
+console.log('First Name: ' + firstName);
+console.log('Last Name: ' + lastName);
+console.log('Address: ' + address);
+console.log('Pay Rate: ' + payRate.toFixed(1));
+console.log('Hire Date: ' + hireDate.toLocaleDateString());
