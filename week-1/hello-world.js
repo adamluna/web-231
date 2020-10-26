752f2c0ed0dd0804412c6c941263bb4d7e81b183
Adam Luna <adluna@my365.bellevue.edu>
2020-10-25 20:15:34 -0500

    add attribution

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,11 @@
+/* 
+========================================== 
+; Title: Exercise 1.3
+; Author: Adam Luna
+; Date: 24 October 2020 
+; Modified By: Adam Luna 
+; Description: Hello World JavaScript assignment for WEB231.
+; ======================================== 
+*/
+
 console.log("Hello World, my name is Adam Luna");
