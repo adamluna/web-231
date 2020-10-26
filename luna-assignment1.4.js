f353f3761c7efcb88dfe381b94ef0af50acfef46
Adam Luna <adluna@my365.bellevue.edu>
2020-10-25 20:25:57 -0500

    add 1.4 assignment

---
+++ luna-assignment1.4.js
@@ -0,0 +1,23 @@
+/* 
+========================================== 
+; Title: Assignment 1.3
+; Author: Adam Luna
+; Date: 25 October 2020 
+; Modified By: Adam Luna 
+; Description: Primitive Types JavaScript assignment for WEB231.
+; ======================================== 
+*/
+
+var firstName = 'Adam';
+var lastName = 'Luna';
+var age = 31;
+var street = '123 Cool Street'
+var city = 'Great';
+var zipCode = 98765;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
