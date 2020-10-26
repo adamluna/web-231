2a5be3c7c7795cc9f0b0e8f6c5b95bcb4324882d
Adam Luna <adluna@my365.bellevue.edu>
2020-10-25 20:42:20 -0500

    add discussion 1.1

---
+++ week-1/luna-discussion-1.1.js
@@ -0,0 +1,16 @@
+/* 
+========================================== 
+; Title: Discussion 1.1
+; Author: Adam Luna 
+; Date: 22 October 2020 
+; Modified By: Adam Luna 
+; Description: Create a simple JavaScript program using values and variables with at least two errors.
+; ======================================== 
+*/
+
+// declaring two variables and printing them together
+var firstName = 'Adam';
+
+var last Name = 'Luna';
+
+console.log('firstName' + lastName);
