226baf10928613a06c1201f139b1f6abfbd3e200
Adam Luna <adluna@my365.bellevue.edu>
2020-11-01 12:26:28 -0600

    add exercise 2.2 files

---
+++ week-2/luna-header-test.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  luna-header-test.js
+; Author: Adam Luna
+; Date:   1 November 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// require reads luna-header.js file and executes it
+const header = require('./luna-header.js');
+
+// display function output
+console.log(header.display("Adam", "Luna", "Exercise 2.2 - Header"));
