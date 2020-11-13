d666aed717712000c35aedfb257d1d901ba5d586
Adam Luna <adluna@my365.bellevue.edu>
2020-11-12 20:41:30 -0600

    Add week 4 folder

---
+++ week-4/luna-discussion-4.1.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title:  luna-discussion-4.1.js
+; Author: Professor Krasso
+; Date:   12 November 2020
+; Modified By: Adam Luna
+; Description: Array program with at least two errors
+;===========================================
+*/
+
