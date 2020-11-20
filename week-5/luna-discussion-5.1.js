ee3b7c27f19aa419e8ffc097da727a3522f9a23d
Adam Luna <adluna@my365.bellevue.edu>
2020-11-19 19:41:06 -0600

    add week 5 folder

---
+++ week-5/luna-discussion-5.1.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title:  luna-discussion-5.1.js
+; Author: Professor Krasso
+; Date:   19 November 2020
+; Modified By: Adam Luna
+; Description: Create a simple JavaScript program using a keyed collection array with at least two (2) errors.
+;===========================================
+*/
+
