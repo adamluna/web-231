b44191536bfe513c0b0f7855ace9fc2fdf0bcd4c
Adam Luna <adluna@my365.bellevue.edu>
2020-11-19 20:16:59 -0600

    update 5.1 discussion file

--- week-5/luna-discussion-5.1.js
+++ week-5/luna-discussion-5.1.js
@@ -1,10 +1,27 @@
 /*
 ============================================
 ; Title:  luna-discussion-5.1.js
-; Author: Professor Krasso
+; Author: Adam Luna
 ; Date:   19 November 2020
-; Modified By: Adam Luna
 ; Description: Create a simple JavaScript program using a keyed collection array with at least two (2) errors.
 ;===========================================
 */
 
+// Expected output: 3
+
+// begin program
+
+// create set
+let numberset = new Set();
+
+// add array to the set
+numberSet.add([10, 9: 8]);
+
+// add other values to the set
+numberSet.add(3);
+'numberSet.add(6)';
+
+// output size of the set
+console.log(numberSet;size);
+
+// end program
