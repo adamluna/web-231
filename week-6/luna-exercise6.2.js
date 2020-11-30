00ecb879fe341cb4e0f3ae2fad73026cb7fbfaa9
Adam Luna <adluna@my365.bellevue.edu>
2020-11-29 21:54:25 -0600

    Add exercise 6.2

---
+++ week-6/luna-exercise6.2.js
@@ -0,0 +1,33 @@
+/*
+============================================
+; Title:  luna-assignment5.4.js
+; Author: Professor Krasso
+; Date:   29 November 2020
+; Modified By: Adam Luna
+; Description: Exception Handling
+;===========================================
+*/
+
+// require reads luna-header.js file and executes it
+const header = require('../luna-header.js');
+
+// display function output
+console.log(header.display("Adam", "Luna", "Assignment 5.4"));
+
+// create a blank line between header and output
+console.log("");
+
+// begin program
+// The total of x and y must be greater than 10 or an error is thrown
+try {
+  let x = 6;
+  let y = 1;
+  var total = x + y;
+if (total < 10) throw "Number must not be less than 10.";
+  console.log(total);
+} catch (err) {
+  console.log("Catch clause: " + err);
+} finally {
+  console.log("Finally clause reached...");
+}
+// end program
